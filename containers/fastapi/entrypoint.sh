56a2753917a6733dbad888076c38295fc0139b86
Yuto Muroshima <yuto_muroshima@YutonoMacBook-Pro.local>
2024-02-23 22:04:01 +0900

    ビルド時にマイグレーションファイルを生成しないように生成しないように修正

--- containers/fastapi/entrypoint.sh
+++ containers/fastapi/entrypoint.sh
@@ -2,11 +2,5 @@
 
 set -eu
 
-# マイグレーションを実行
-echo "Running migrations..."
-poetry run alembic revision --autogenerate
 poetry run alembic upgrade head
-
-# アプリケーションを起動
-echo "Starting the application..."
 poetry run uvicorn main:app --reload --host 0.0.0.0 --port 8000
