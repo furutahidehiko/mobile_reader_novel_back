da9b64a8e4083218f658938f88ecfd70b9c14536
taku ikegami <takuron1996@gmail.com>
2024-03-09 09:33:30 +0900

    typo修正

--- application/command.py
+++ application/command.py
@@ -19,6 +19,6 @@ from command import load_json
 if __name__ == "__main__":
     args = sys.argv
     if len(args) < 2:
-        print("実行するコマンドを指定していください", file=sys.stderr)
+        print("実行するコマンドを指定してください", file=sys.stderr)
     if args[1] == "load_json":
         asyncio.run(load_json.run(args))
